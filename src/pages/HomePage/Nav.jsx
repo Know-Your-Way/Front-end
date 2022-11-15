8f7c8cc0aba2448ecbd2b6e3253a30e46478d929
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2022-11-15 22:28:08 +0100

    asset folder with tailwind config file update

--- src/pages/HomePage/Nav.jsx
+++ src/pages/HomePage/Nav.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <nav className=' container mx-auto mt-5 flex justify-between'>
-            <div>
-                <Link><h1 className='leading-none italic'>Know</h1></Link>
-                <Link><h1 className=' font-bold leading-none'>Your Way</h1></Link>
-            </div>
-            <ul className='flex'>
-                <li><Link to='/'>Home</Link></li>
-                <li className='pl-8'><Link to='/about'>About</Link></li>
-                <li className='pl-8'><Link to='/contact'>Contact</Link></li>
-            </ul>
-        </nav>
+        <header>
+            <nav className=' container mx-auto py-5 flex justify-between'>
+                <div>
+                    <Link to='/'><h1 className='leading-none italic'>Know</h1></Link>
+                    <Link to='/'><h1 className=' font-bold leading-none'>Your Way</h1></Link>
+                </div>
+                <div className='flex space-x-8'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/contact'>Contact</Link>
+                </div>
+            </nav>
+        </header>
     )
 }
 

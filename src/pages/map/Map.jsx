07ee99fda4799d729e35eb78e2757c50fd5c14e9
EliasMlopez99 <realcometsmash@gmail.com>
2022-11-19 17:04:23 +0100

    functional map with marker added

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
 
 
 
 const Map = () => {
-const position = [52.51, 13.38]
+
+const position = [50.846, 4.3518]
 
 return (
-  <section className="h-200">
-<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+  <section className="min-h-screen min-w-full leaflet-container">
+<MapContainer className="min-h-screen min-w-full"
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+    //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={position}>
+  <Marker position={position}
+  icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
     <Popup>
-      Hola <br /> Easily customizable.
+      Grand Place <br /> The Grand-Place is the central square of the City of Brussels. All over the world it is known for its decorative and aesthetic wealth.
     </Popup>
   </Marker>
-</MapContainer></section>
+</MapContainer>
+</section>
   )
 }
 

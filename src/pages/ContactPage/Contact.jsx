b22ef8b4baffaed92d1fd6639d86a2ff1d532339
EliasMlopez99 <realcometsmash@gmail.com>
2022-11-25 12:14:57 +0100

    added the contact form feature

--- src/pages/ContactPage/Contact.jsx
+++ src/pages/ContactPage/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rpw285v', 'contact_form', form.current, 'Y39z6YlGoKk14XZc_')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
   return (
     <div className="container mx-auto bg-contact">
-      <section className="text-gray-600 body-font relative">
+      <section ref={form} onSubmit={sendEmail} className="text-gray-600 body-font relative">
         <div className="container px-5 py-24 mx-auto flex">
           <img src='/Asset/Contact.svg' alt='hero_img' className='object-contain' />
           <div className="lg:w-1/3 md:w-1/2 bg-gray-500 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
@@ -12,21 +28,30 @@ const Contact = () => {
             </h2>
             <div className="relative mb-4">
               <label for="email" className="leading-7 text-sm text-white">
-                First name
+              First name
               </label>
               <input
-                type="email"
+                type="text"
                 id="email"
-                name="email"
+                name="from_name"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               ></input>
               <label for="email" className="leading-7 text-sm text-white">
-                Last name
+               Last name
               </label>
               <input
-                type="email"
+                type="text"
                 id="email"
-                name="email"
+                name="last_name"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              ></input>
+               <label for="email" className="leading-7 text-sm text-white">
+              Telephone number
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="contact_number"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               ></input>
               <label for="email" className="leading-7 text-sm text-white">
@@ -35,7 +60,7 @@ const Contact = () => {
               <input
                 type="email"
                 id="email"
-                name="email"
+                name="user_email"
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               ></input>
             </div>
@@ -50,7 +75,7 @@ const Contact = () => {
               ></textarea>
             </div>
             <button className="text-gray-600 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-              Button
+            <input type="submit" value="Send" />Button
             </button>
           </div>
         </div>

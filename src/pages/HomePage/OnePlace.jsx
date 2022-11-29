4bf163a65c534a880271dc2d00ed521964b17677
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2022-11-29 13:30:55 +0100

    fixed the oneplace issue

--- src/pages/HomePage/OnePlace.jsx
+++ src/pages/HomePage/OnePlace.jsx
@@ -1,49 +1,58 @@
 import React, { useEffect, useState } from 'react'
-import {useParams , useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 
-// const baseApi = 'http://localhost:1337';
+const baseApi = 'http://localhost:1337';
 
 const OnePlace = () => {
 
-    const [place, setPlace] = useState({})
+    const [place, setPlace] = useState({
+        attributes: {
+            image: {
+                data: {
+                    attributes: {
+                        url: '',
+                    },
+                },
+            },
+        },
+    });
+
+
+    const [param, setParam] = useState(null)
     const params = useParams()
-    // const [paramState, setParamState] = useState(1)
     const navigate = useNavigate()
     console.log(place)
-    console.log('1', params)     
+    console.log('1', params)
 
     useEffect(() => {
-        console.log('inside effect', params);     
+        console.log('inside effect', params);
         fetch(`http://localhost:1337/api/landmarks/${params.id}?populate=*`)
             .then(res => res.json())
             .then(landmark => {
                 console.log(landmark, 'landmark')
                 setPlace(landmark.data)
-                // setParamState(params)
+
             })
 
-    }, [params])
+    }, [])
 
     return (
         <div className=' container mx-auto lg:ml-80 mt-20 mb-10'>
-            <button className='mb-12 font-bold' onClick={()=>{navigate("/")}}>Back</button>
+            <button className='mb-12 font-bold' onClick={() => { navigate("/") }}>Back</button>
             <div className=" max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <img className="object-cover w-full h-64" alt="Article" />
+                <img className="object-cover w-full h-64" src={`${baseApi}${place.attributes.image.data.attributes.url}`} alt="Article" />
                 <div className="p-6">
                     <div>
                         <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">Most visited place</span>
                         <h5 className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline" role="link"
                         >{place.attributes.Title}</h5>
-                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">hiii</p>
+                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">{place.attributes.Description}</p>
                     </div>
                 </div>
             </div>
         </div>
-)}
+    )
+}
 
 export default OnePlace
-
-// src={`${baseApi}${place.attributes.image.data.attributes.url}`}
-// {place.attributes.Title}
-// {place.attributesDescription}

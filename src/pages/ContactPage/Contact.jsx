68412a0219a8a97d85f1ca9b9b85f6b9efa44af6
Ayomide <heritageayomide2017@gmail.com>
2022-12-04 20:36:37 +0100

    fixed responsiveness issue on contact us page

--- src/pages/ContactPage/Contact.jsx
+++ src/pages/ContactPage/Contact.jsx
@@ -34,18 +34,18 @@ const Contact = () => {
   return (
 
     <section className=" bg-contact pb-10">
-      <div className=" container  mx-auto">
-        <h2 className=" pt-5 md:ml-8 md:text-3xl mb-4 font-medium text-white">
+      <div className=" container mx-auto">
+        <h2 className=" pt-5 text-center md:ml-8 md:text-3xl mb-4 font-medium text-white">
           Contact us
         </h2>
-        <div className="md:ml-20 ml-10 grid md:grid-cols-2 ">
-          <div className=" bg-gray-600 w-96 p-5 rounded-xl  ">
+        <div className="md:ml-10 grid md:grid-cols-2">
+          <div className=" bg-gray-600 mx-auto p-5 rounded-xl ">
             <form 
               onSubmit={handleSubmit(sendEmail)}
               className="text-gray-60 justify-center">
               <label className="leading-7 text-lg text-white">First Name</label>
               <br />
-              <input className=" w-80 my-1 py-1 rounded-md"
+              <input className="w-full md:w-80 my-1 py-1 rounded-md"
                 name="from_name"
                 {...register("from_name", {
                   required: "this is required",
@@ -66,7 +66,7 @@ const Contact = () => {
               <label className="leading-7 text-lg text-white">Last Name</label>
 
               <br />
-              <input className="w-80 my-1 py-1 rounded-md"
+              <input className="w-full md:w-80 my-1 py-1 rounded-md"
                 {...register("last_name", {
                   required: "this is required",
                   minLength: {
@@ -81,7 +81,7 @@ const Contact = () => {
               <label className="leading-7 text-lg text-white">Telephone Number</label>
 
               <br />
-              <input className="w-80 my-1 py-1 rounded-md"
+              <input className="w-full md:w-80 my-1 py-1 rounded-md"
                 {...register("contact_number", {
                   required: "this is required",
                 })}
@@ -94,7 +94,7 @@ const Contact = () => {
               <label className="leading-7 text-lg text-white">Email</label>
 
               <br />
-              <input className="w-80 my-1 py-1 rounded-md"
+              <input className="w-full md:w-80 my-1 py-1 rounded-md"
                 {...register("user_email", {
                   required: "this field is required",
                   pattern: {
@@ -109,13 +109,13 @@ const Contact = () => {
               <label className="leading-7 text-lg text-white">Message</label>
 
               <br />
-              <textarea className="w-80 my-1 py-1 rounded-md"
+              <textarea className="w-full md:w-80 my-1 py-1 rounded-md"
                 {...register("message", { required: "Please enter a message" })}
                 placeholder="Write your message here"
                 name="message"
               ></textarea>
               <br />
-              <button className="bg-blue-600 my-2 text-white py-2 px-5 rounded-lg ml-60" type="submit">Send</button>
+              <button className="bg-blue-600 my-2 text-white py-2 px-5 rounded-lg md:ml-60" type="submit">Send</button>
             </form>
           </div>
             <img
